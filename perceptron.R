65f058494a5c5a1776c47fa368d0aed6e31408a3
Vasileios Tsakalos <vasilis.m.tsakalos@gmail.com>
2017-01-19 22:00:09 +0000

    Update perceptron.R

--- perceptron.R
+++ perceptron.R
@@ -1,10 +1,10 @@
 perceptron <- function(x, y, lr) {
   # initialize weight vector
-  weight <- c(0.5,0,0,0)
+  weight <- c(0.5,0,0,0) # bias = 0.5 , w = (0,0,0)
   # loop through training data set
   for (j in 1:length(y)){
     z <- 0
-    for (i in 1:3) {
+    for (i in 1:3) { # We have 3 input variables, so we set the loop to 3
       # Predict binary label using step activation function
       z <- z +weight[i+1]*as.numeric(x[i,j])
     }
@@ -14,7 +14,7 @@ perceptron <- function(x, y, lr) {
     } else {
       ypred <- 1
     }
-    for (i in 1:3){
+    for (i in 1:3){ # We have 3 input variables, so we set the loop to 3
       weight[i+1] <- weight[i+1] + lr * (y[j] - ypred) *as.numeric(x[i,j])
     }
     weight [1] <- weight[1] + lr * (y[j] - ypred)
